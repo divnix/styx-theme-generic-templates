60badc5b1de72cf3d8667c61fa9294fb5f596602
David Arnold <david.arnold@iohk.io>
2022-08-12 17:19:35 -0500

    refactor: move to divnix orga for further / temporary maintenance

--- example/conf.nix
+++ example/conf.nix
@@ -1,7 +1,7 @@
 {
   /* URL of the site, must be set to the url of the domain the site will be deployed
   */
-  siteUrl = "https://styx-static.github.io/styx-theme-generic-templates";
+  siteUrl = "https://divnix.github.io/styx-theme-generic-templates";
 
   /* Theme specific settings
      it is possible to override any of the used themes configuration in this set
--- meta.nix
+++ meta.nix
@@ -4,8 +4,8 @@
   name = "Generic templates";
   license = lib.licenses.mit;
   maintainers = with lib.maintainers; [ ericsagnes ];
-  demoPage = https://styx-static.github.io/styx-theme-generic-templates;
-  homepage = https://github.com/styx-static/styx-theme-generic-templates;
+  demoPage = https://divnix.github.io/styx-theme-generic-templates;
+  homepage = https://github.com/divnix/styx-theme-generic-templates;
   tags = [ "generic-templates" ];
   screenshot = ./screen.png;
   documentation = lib.readFile ./documentation.adoc;
